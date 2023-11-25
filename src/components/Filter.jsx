a8abadeff2ba131dbf444a74ccd1aa8e50feff4b
Stalin <sta_vla411@hotmail.com>
2023-11-24 22:13:00 -0500

    add icon and styles

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,12 +1,34 @@
 function Filter() {
   return (
-    <section className='flex gap-4'>
-      <input type='text' />
-      <button className='bg-dark-light p-1 rounded'>
+    <section className='flex gap-4 w-full justify-between items-center'>
+      <div className='w-full relative'>
+        <svg
+          className='absolute top-2 left-2'
+          xmlns='http://www.w3.org/2000/svg'
+          width='18'
+          height='18'
+          viewBox='0 0 24 24'
+          strokeWidth='2'
+          stroke='currentColor'
+          fill='none'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        >
+          <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+          <path d='M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0' />
+          <path d='M21 21l-6 -6' />
+        </svg>
+        <input
+          className='w-full focus-visible:outline-none rounded pr-2 pl-8 py-1 bg-gray-50 dark:bg-dark-light text-slate-900 dark:text-slate-100'
+          type='text'
+          placeholder='Buscar...'
+        />
+      </div>
+      <button className='bg-dark-light rounded p-1'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
-          width='26'
-          height='26'
+          width='24'
+          height='24'
           viewBox='0 0 24 24'
           strokeWidth='2'
           stroke='currentColor'

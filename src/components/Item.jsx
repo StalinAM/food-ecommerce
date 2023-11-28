4869388d15b71254c9d14883fde17e9356f74433
stalin <sta_vla411@hotmail.com>
2023-11-27 20:12:00 -0500

    refactor code

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,20 +1,22 @@
 function Item() {
   return (
-    <article className='p-3 dark:bg-dark-light rounded-md relative overflow-hidden flex flex-col gap-2'>
+    <article className='p-3 dark:bg-dark-light rounded-md relative overflow-hidden flex flex-col gap-3'>
       <img
         src='https://the-weirdos.netlify.app/static/media/bighead-3.ee360106bab5dce5e9d68a2a0ea516ad.svg'
         alt=''
       />
-      <header>
-        <h3 className='font-medium dark:text-slate-100 text-sm'>
-          Pizza de piña
-        </h3>
-      </header>
-      <div className='flex flex-col leading-5 dark:text-slate-100 font-bold text-lg'>
-        <span className='text-sm dark:text-slate-100/60 font-normal'>
-          20,00 $
-        </span>
-        <span>15,00 $</span>
+      <div>
+        <header>
+          <h3 className='font-medium dark:text-slate-100 text-sm mb-1'>
+            Pizza de piña
+          </h3>
+        </header>
+        <div className='flex flex-col leading-5 dark:text-slate-100 font-bold text-lg'>
+          <span className='text-xs dark:text-slate-100/60 font-normal'>
+            $20.00
+          </span>
+          <span>$15.00</span>
+        </div>
       </div>
       <button className='absolute dark:bg-slate-50 rounded-tl-md bottom-0 right-0 w-10 h-10'>
         <svg

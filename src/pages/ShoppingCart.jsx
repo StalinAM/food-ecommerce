81dd287b8da266343db28e25f4c0e4615b0f28ce
stalin <sta_vla411@hotmail.com>
2023-12-03 00:03:53 -0500

    add send button and change style to title

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -3,7 +3,7 @@ import ItemCart from '../components/ItemCart'
 function ShoppingCart() {
   return (
     <section className='flex flex-col gap-4'>
-      <h2>Shopping cart</h2>
+      <h2 className='dark:text-slate-100 font-medium'>Shopping cart</h2>
       <ul>
         <li className='mb-4'>
           <ItemCart />
@@ -21,6 +21,9 @@ function ShoppingCart() {
           <ItemCart />
         </li>
       </ul>
+      <button className='border-2 dark:hover:bg-transparent dark:hover:text-slate-100 dark:bg-slate-100 dark:text-dark font-medium text-lg rounded-md py-1'>
+        Enviar pedido
+      </button>
     </section>
   )
 }

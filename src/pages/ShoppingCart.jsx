9d2f93439ba97af9a2f9205baaf1f21bb4226ced
stalin <sta_vla411@hotmail.com>
2023-12-02 23:14:40 -0500

    refactor code and update styles

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -2,10 +2,22 @@ import ItemCart from '../components/ItemCart'
 
 function ShoppingCart() {
   return (
-    <section>
+    <section className='flex flex-col gap-4'>
       <h2>Shopping cart</h2>
       <ul>
-        <li>
+        <li className='mb-4'>
+          <ItemCart />
+        </li>
+        <li className='mb-4'>
+          <ItemCart />
+        </li>
+        <li className='mb-4'>
+          <ItemCart />
+        </li>
+        <li className='mb-4'>
+          <ItemCart />
+        </li>
+        <li className='mb-4'>
           <ItemCart />
         </li>
       </ul>

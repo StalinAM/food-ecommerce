081dc422bccb1affa6d037eb3f5d5d906a32a123
Stalin <sta_vla411@hotmail.com>
2023-12-04 18:50:58 -0500

    update home

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 
 import Home from './pages/Home'
 import ShoppingCart from './pages/ShoppingCart'
+import ItemDetails from './pages/ItemDetails'
 
 function App() {
   const [activeMenu, setActiveMenu] = useState(false)
@@ -10,8 +11,9 @@ function App() {
     <>
       <Header activeMenu={activeMenu} setActiveMenu={setActiveMenu} />
       <main className='px-4 pt-2 pb-4 w-full flex flex-col gap-6'>
-        <Home />
+        {/* <Home /> */}
         {/* <ShoppingCart /> */}
+        <ItemDetails />
       </main>
     </>
   )

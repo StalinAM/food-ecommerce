f0841304a52d75fd05dc679bca1687aef0103353
StalinAM <sta_vla411@hotmail.com>
2023-12-09 21:15:04 -0500

    add new categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,9 +2,27 @@ function Categories() {
   return (
     <form>
       <h2 className='mb-3'>Categories</h2>
-      <div>
+      <div className='flex gap-3'>
         <label className='cursor-pointer'>
-          <input type='checkbox' className='hidden   peer' name='category' />
+          <input type='radio' className='hidden peer' name='category' />
+          <span className='px-2 py-1 text-sm rounded-md border border-dark-light bg-dark-light peer-checked:border-slate-50 peer-checked:font-medium'>
+            Todos
+          </span>
+        </label>
+        <label className='cursor-pointer'>
+          <input type='radio' className='hidden peer' name='category' />
+          <span className='px-2 py-1 text-sm rounded-md border border-dark-light bg-dark-light peer-checked:border-slate-50 peer-checked:font-medium'>
+            Lettering
+          </span>
+        </label>
+        <label className='cursor-pointer'>
+          <input type='radio' className='hidden peer' name='category' />
+          <span className='px-2 py-1 text-sm rounded-md border border-dark-light bg-dark-light peer-checked:border-slate-50 peer-checked:font-medium'>
+            Games
+          </span>
+        </label>
+        <label className='cursor-pointer'>
+          <input type='radio' className='hidden peer' name='category' />
           <span className='px-2 py-1 text-sm rounded-md border border-dark-light bg-dark-light peer-checked:border-slate-50 peer-checked:font-medium'>
             Anime
           </span>
